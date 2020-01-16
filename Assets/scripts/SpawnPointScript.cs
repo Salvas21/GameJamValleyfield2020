0f8b789202a81b21d71978f653c102d98eca13cf
BananaRaptor <arnaudthebergemandeville@gmail.com>
2020-01-16 03:56:02 -0500

    Mege merge part 2

--- Assets/scripts/SpawnPointScript.cs
+++ Assets/scripts/SpawnPointScript.cs
@@ -1,20 +1,22 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
+//using Random = System.Random;
 
 public class SpawnPointScript : MonoBehaviour
 {
-    
-    //public Transform[] spawnPoints = new Transform[5];
     public List<Vector3> spawnPoints = new List<Vector3>();
-    public List<GameObject> itemsToAdd = new List<GameObject>();
 
     public GameObject leadBullet;
     public GameObject silverBullet;
     public GameObject lantern;
     public GameObject key;
+
+    public GameObject barrier;
+
+    public int numberOfKeys = 0;
 
 
     // Start is called before the first frame update
@@ -27,16 +29,29 @@ public class SpawnPointScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (numberOfKeys == 3)
+        {
+            Destroy(barrier);
+        }
     }
 
     private void initSpawnPoints()
     {
-        spawnPoints[0] = new Vector3(-82, 31, 0);
+        spawnPoints.Add(new Vector3(-81, 31, 0));
+        spawnPoints.Add(new Vector3(19, 48, 0));
+        spawnPoints.Add(new Vector3(30, 14, 0));
+        spawnPoints.Add(new Vector3(81, 22, 0));
+        spawnPoints.Add(new Vector3(-104, 36, 0));
+        spawnPoints.Add(new Vector3(-167, 43, 0));
+        spawnPoints.Add(new Vector3(-179, 128, 0));
+        
+        /*spawnPoints[0] = new Vector3(-81, 31, 0);
         spawnPoints[1] = new Vector3(19, 48, 0);
         spawnPoints[2] = new Vector3(30, 14, 0);
         spawnPoints[3] = new Vector3(81, 22, 0);
-        
+        spawnPoints[4] = new Vector3(-104, 36, 0);
+        spawnPoints[5] = new Vector3(-167, 43, 0);
+        spawnPoints[6] = new Vector3(-179, 128, 0);*/
     }
 
     private void determineItemSpawn()
@@ -44,34 +59,38 @@ public class SpawnPointScript : MonoBehaviour
         int keyAmount = 0;
         bool lanternSpawned = false;
         int item = 0;
+        
         for (int i = 0; i < spawnPoints.Count; i++)
         {
-            item = 1;
+            if (spawnPoints.Count - i <= 3 - keyAmount && keyAmount < 3)
+            {
+                item = 0;
+            }
+            else
+            {
+                item = Random.Range(1, 3);
+            }
             if (item == 0 && keyAmount < 3)
             {
                 // key
-                itemsToAdd.Add(Instantiate(this.key, spawnPoints[i], transform.rotation));
-                spawnPoints.RemoveAt(i);
+                Instantiate(key, spawnPoints[i], transform.rotation);
+                keyAmount++;
+                numberOfKeys++;
             } else if (item == 1 && !lanternSpawned)
             {
                 // lantern
-                //itemsToAdd.Add(Instantiate(this.lantern, spawnPoints[i], transform.rotation));
                 Instantiate(lantern, spawnPoints[i], transform.rotation);
-                spawnPoints.RemoveAt(i);
                 lanternSpawned = true;
             }
             else if (item == 2) 
             {
                 // silver bullet
-                itemsToAdd.Add(Instantiate(this.silverBullet, spawnPoints[i], transform.rotation));
-                spawnPoints.RemoveAt(i);
+                Instantiate(this.silverBullet, spawnPoints[i], transform.rotation);
             }
             else
             {
                 // lead bullet
-                //itemsToAdd.Add(Instantiate(this.leadBullet, spawnPoints[i], transform.rotation));
                 Instantiate(leadBullet, spawnPoints[i], transform.rotation);
-                spawnPoints.RemoveAt(i);
             }
         }
     }
