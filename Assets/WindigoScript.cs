c7b25ce5f0790441ef45bc03723dde7785c51dfd
Max <maxlamenace_2000@hotmail.com>
2020-01-16 01:59:30 -0500

    Pathfinding Fix and More Fixes

--- Assets/WindigoScript.cs
+++ Assets/WindigoScript.cs
@@ -1,21 +1,7 @@
 ﻿using UnityEngine;
 
 public class WindigoScript : MonoBehaviour
-
-    
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.name == "Bullet")
